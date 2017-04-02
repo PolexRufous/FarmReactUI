d2fbd59e8be492acb3192c98b01720e07fed249d
PolexRufous <polex.rufous@gmail.com>
2017-04-03 00:29:27 +0300

    Fix i18n

---
+++ src/i18n/ru.js
@@ -0,0 +1,29 @@
+export default {
+    locale: "ru",
+    pluralRuleFunction:function(e,a){var t=String(e).split("."),r=t[0],o=!t[1],n=r.slice(-1),l=r.slice(-2);return a?"other":o&&1==n&&11!=l?"one":o&&n>=2&&n<=4&&(l<12||l>14)?"few":o&&0==n||o&&n>=5&&n<=9||o&&l>=11&&l<=14?"many":"other"},
+    messages: {
+        "PAY_SALARY_CASH_UA": "зарплата",
+        "CONTACT_US_HEADER": "Наши контакты",
+        "CONTACT_US_TEXT": "Какой-то текст про контакты",
+        "NAVIGATION_HEADER": "Супер Ферма",
+        "NAVIGATION_MENU_MAIN": "Главная",
+        "NAVIGATION_MENU_NOT_MAIN": "Не главная",
+        "LEFT_SIDE_HEADER": "Заголовок слева",
+        "LEFT_SIDE_TEXT": "Текст слева",
+        "LATEST_OPERATIONS": "Последние операции",
+        "DATE": "Дата",
+        "PARTNER": "Партнер",
+        "PARTNERS": "Партнеры",
+        "OPERATION": "Операция",
+        "AMOUNT": "Сумма",
+        "CHANGE": "Изменить",
+        "MORE": "Дополнительно",
+        "NAME": "Имя",
+        "DESCRIPTION": "Описаине",
+        "ACTIONS": "Действия",
+        "REFRESH": "Обновить",
+        "ADD": "Добавить",
+        "SAVE": "Сохранить",
+        "CANCEL": "Отмена"
+    }
+}
--- src/main.entry.js
+++ src/main.entry.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as GlobalConfig from './global.config.json';
 import { IntlProvider, addLocaleData } from 'react-intl';
-import localeData from './i18n/locale.json';
 
 import Header from './components/header/header';
 import Main from './components/main/main';
 import Footer from './components/footer/footer';
-import ru from './i18n/locale.json';
+import ru from './i18n/ru';
 
 import * as BootstrapCssTheme from '../node_modules/bootstrap-css-only/css/bootstrap-theme.min.css';
 import * as BootstrapCss from '../node_modules/bootstrap-css-only/css/bootstrap.min.css';
@@ -18,13 +17,13 @@ const language = (navigator.languages && navigator.languages[0]) ||
     navigator.language ||
     navigator.userLanguage;
 const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
-const messages = localeData[languageWithoutRegionCode] || localeData[language] || localeData.ru;
+const messages = ru.messages;
 
 
 const header = document.getElementById(GlobalConfig.headerClassName);
 const main = document.getElementById(GlobalConfig.mainClassName);
 const footer = document.getElementById(GlobalConfig.footerClassName);
 
-ReactDOM.render(<IntlProvider locale={language} messages={messages}><Header /></IntlProvider>,header);
-ReactDOM.render(<IntlProvider locale={language} messages={messages}><Main /></IntlProvider>,main);
-ReactDOM.render(<IntlProvider locale={language} messages={messages}><Footer /></IntlProvider>,footer);
+ReactDOM.render(<IntlProvider locale={languageWithoutRegionCode} messages={messages}><Header /></IntlProvider>,header);
+ReactDOM.render(<IntlProvider locale={languageWithoutRegionCode} messages={messages}><Main /></IntlProvider>,main);
+ReactDOM.render(<IntlProvider locale={languageWithoutRegionCode} messages={messages}><Footer /></IntlProvider>,footer);
